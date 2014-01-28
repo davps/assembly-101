f125d9a231f78f21f82800510f5ef36f49501539
davps <davidperez3010@gmail.com>
2014-01-27 23:36:32 -0300

    Adding a boilerplate code for assembler programs

---
+++ assembler_boilerplate.asm
@@ -0,0 +1,55 @@
+%include "lib/asm_io.inc"
+segment .data
+;
+; los datos indicados
+; Ejemplo: 
+; Message db "Ingrese un numero: ", 0
+;
+
+segment .bss
+;
+; los datos no iniciados se incluyen aqui
+; Ejemplo:
+; Input resd 1
+;
+
+segment .text
+        global asm_main
+asm_main:
+        enter 0,0 ;setup routine
+        pusha
+
+;
+; Aqui va el codigo del programa
+;
+
+        popa
+        mov eax, 0 	;retornar a C
+        leave
+        ret  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
