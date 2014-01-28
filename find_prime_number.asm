6d9a43e84cbf655e3733067f362cb0d7ba7d02af
davps <davidperez3010@gmail.com>
2014-01-28 00:40:50 -0300

    Adding a little step on this program

--- find_prime_number.asm
+++ find_prime_number.asm
@@ -1,10 +1,13 @@
 %include "lib/asm_io.inc"
 segment .data
-Message db "Find prime up to: ", 0
+Message db "Hallar los numeros primos hasta: ", 0
+while_limit_str db "enter while_limit", 0
+while_factor_str db "enter while_factor", 0
 
 segment .bss
-Limit resd 1 ;find primes up to this limit
-Guess resd 1 ;the current guess for prime
+Limit resd 1 ;hallar los numeros primos hasta este limite
+Guess resd 1 ;la conjetura actual para el primo
+
 
 segment .text
         global asm_main
@@ -14,25 +17,49 @@ asm_main:
 
         mov eax, Message
         call print_string
-        call read_int
+        call read_int 				; scanf("%u", & limit);
         mov [Limit], eax
+
+						; printf("2\n");
         mov eax, 2
         call print_int
         call print_nl
+
+						; printf("3\n");
         mov eax, 3
         call print_int
         call print_nl
-        mov dword [Guess], 5 ; Guess = 5
+
+        mov dword [Guess], 5 			; Guess = 5
+
+	;dump_regs 1
+
 while_limit: 
+						;debugging the esta entrando al condicional del bucle
+	mov ecx, eax
+	mov eax, while_limit_str
+	call print_string
+	call print_nl
+	mov eax, ecx
+	;mov edx, [Guess]
+
+						;if(Guess == limit)
         mov eax, [Guess]
         cmp eax, [Limit]
+	dump_regs 1				;debugging registers
         jnbe end_while_limit
 
         mov ebx, 3   
 while_factor:
-       mov eax, ebx
-       mul eax
-       jo end_while_factor
+	mov ecx, eax
+	mov eax, while_factor_str
+	call print_string
+	call print_nl
+	mov eax, ecx
+
+        mov eax, ebx
+        mul eax
+        jo end_while_factor
 
 end_while_factor:
        je end_if
